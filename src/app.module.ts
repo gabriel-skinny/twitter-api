cbc02fb5d04d91a380c002e506080b9c1ad6b1a0
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-08-15 11:27:42 -0300

    Making mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,14 @@ import HttpModule from '@infra/http/http.module';
 import { ServiceModule } from '@infra/services/service.module';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import 'dotenv/config';
+
+const mongoUrl = `mongodb://${process.env.MONGODB_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_DB_HOST}:${process.env.MONGODB_LOCAL_PORT}/${process.env.MONGODB_DB_NAME}?authSource=admin`;
 
 @Module({
   imports: [
     HttpModule,
-    MongooseModule.forRoot('mongodb://localhost:27017'),
+    MongooseModule.forRoot(mongoUrl),
     DatabaseModule,
     ServiceModule,
   ],
