e9cd9b001e1d366b9c2a7720259a698c82763979
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-08-15 08:50:27 -0300

    Adding new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
+import { DatabaseModule } from '@infra/database/database.module';
+import HttpModule from '@infra/http/http.module';
+import { ServiceModule } from '@infra/services/service.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    HttpModule,
+    MongooseModule.forRoot('mongodb://localhost:27017'),
+    DatabaseModule,
+    ServiceModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
