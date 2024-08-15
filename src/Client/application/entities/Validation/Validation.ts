bf1ce0e806bf3ace2ddc23ca8905e7617bd2d171
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-08-15 15:32:35 -0300

    removing non used class

--- src/Client/application/entities/Validation/Validation.ts
+++ src/Client/application/entities/Validation/Validation.ts
@@ -1,86 +1,77 @@
-import { EXPIRES_EMAIL_VALIDATION_IN_MINUTES } from "src/Client/application/constants/emailValidation";
-import { randomUUID } from "crypto";
-import { ValidationCode } from "./ValidationCode";
-import { BaseExpiresIn } from "../base/baseExpiresIn";
-import { ValidationAttempt } from "./ValidationAttempt";
+import { randomUUID } from 'crypto';
+import { EXPIRES_EMAIL_VALIDATION_IN_MINUTES } from 'src/Client/application/constants/emailValidation';
+import { BaseExpiresIn } from '../base/baseExpiresIn';
+import { ValidationCode } from './ValidationCode';
 
 export enum OperationToValidateTypeEnum {
-	EMAIL_CONFIRMATION = "email_confirmation",
-	PASSWORD_CHANGE = "password_change"
+  EMAIL_CONFIRMATION = 'email_confirmation',
+  PASSWORD_CHANGE = 'password_change',
 }
 
 interface IValidationProps {
-	id?: string;
-	validated?: boolean;
-	operationToValidateType: OperationToValidateTypeEnum; 
-    email: string;
-    validationCode: ValidationCode;
-	validationAttempt?: ValidationAttempt;
-	expirationIn?: number;
-    createdAt?: Date;
+  id?: string;
+  validated?: boolean;
+  operationToValidateType: OperationToValidateTypeEnum;
+  email: string;
+  validationCode: ValidationCode;
+  expirationIn?: number;
+  createdAt?: Date;
 }
 
-type IRawValues = Omit<IValidationProps, 
-"id" | "createdAt" | "validated" | "expirationIn"
+type IRawValues = Omit<
+  IValidationProps,
+  'id' | 'createdAt' | 'validated' | 'expirationIn'
 >;
 
-export class Validation extends BaseExpiresIn{
-	private _id: string;
-	private _validated: boolean;
-	private _validationCode: ValidationCode;
-	private _validationAttempt: ValidationAttempt;
-	private rawValues: IRawValues;
+export class Validation extends BaseExpiresIn {
+  private _id: string;
+  private _validated: boolean;
+  private _validationCode: ValidationCode;
+  private rawValues: IRawValues;
 
- constructor(props: IValidationProps) {
-	super({ 
-		expiresInMinutesFixedValue: EXPIRES_EMAIL_VALIDATION_IN_MINUTES,
-		expiresInValue: props.expirationIn,
-		createdAt: props.createdAt
-	})
+  constructor(props: IValidationProps) {
+    super({
+      expiresInMinutesFixedValue: EXPIRES_EMAIL_VALIDATION_IN_MINUTES,
+      expiresInValue: props.expirationIn,
+      createdAt: props.createdAt,
+    });
 
-	this._id = props.id || randomUUID();
-	this._validated = props.validated || false;
-	this._validationCode = props.validationCode;
+    this._id = props.id || randomUUID();
+    this._validated = props.validated || false;
+    this._validationCode = props.validationCode;
 
-	this.rawValues = { ...props }
+    this.rawValues = { ...props };
+  }
 
-	this._validationAttempt = props.validationAttempt || new ValidationAttempt({ validationId: this._id });
- }
+  public get id() {
+    return this._id;
+  }
 
- public get id() {
-	return this._id;
- }
+  public validate() {
+    this._validated = true;
+  }
 
- public validate() {
-	this._validated = true;
- }
+  public get validated() {
+    return this._validated;
+  }
 
- public get validated() {
-	return this._validated;
- }
+  public get email() {
+    return this.rawValues.email;
+  }
 
- public get email() {
-	return this.rawValues.email;
- }
+  public set email(email: string) {
+    this.rawValues.email = email;
+  }
 
- public set email(email: string) {
-	this.rawValues.email = email;
- }
+  public get validationCode() {
+    return this._validationCode;
+  }
 
- public get validationCode() {
-	return this._validationCode;
- }
+  public get operationToValidateType() {
+    return this.rawValues.operationToValidateType;
+  }
 
- public get validationAttempt() {
-	return this._validationAttempt;
- }
-
- public get operationToValidateType() {
-	return this.rawValues.operationToValidateType;
- }
-
- public set validationCode(validationCode: ValidationCode) {
-	this._validationCode = validationCode;
- }
-
+  public set validationCode(validationCode: ValidationCode) {
+    this._validationCode = validationCode;
+  }
 }
--- src/Client/application/repositories/validationAttempt/validationAttempt.ts
+++
@@ -1,6 +0,0 @@
-import { ValidationAttempt } from "../../entities/Validation/ValidationAttempt";
-
-export default abstract class AbstractValidationAttemptRepository {
-    abstract save(validationAttempt: ValidationAttempt): Promise<void>; 
-    abstract findByValidationId(validationId: string): Promise<ValidationAttempt | null>;
-}
