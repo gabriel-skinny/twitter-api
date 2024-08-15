4593f197c4ab5bb66d100faa16e7de28721e2600
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-08-15 11:29:22 -0300

    Removing comments

--- src/Client/application/use-cases/register-flow/startAccount/start-account-use-case.ts
+++ src/Client/application/use-cases/register-flow/startAccount/start-account-use-case.ts
@@ -1,47 +1,53 @@
-import { OperationToValidateTypeEnum } from "src/Client/application/entities/Validation/Validation";
-import AbstractPreUserRepository from "src/Client/application/repositories/preUser/preUserRepository";
-import { Injectable } from "@nestjs/common";
-import ErrorUserAlreadyCreated from "../../../errors/userAlreadyCreated";
-import AbstractUserRepository from "../../../repositories/user/userRepository";
-import { AbstractCreateValidationCodeUseCase, CreateValidationCodeUseCase } from "../../code-validation/create/create-validation-code";
-import PreUser from "src/Client/application/entities/User/preUser";
-import { Password } from "src/Client/application/entities/User/Password";
+import { OperationToValidateTypeEnum } from 'src/Client/application/entities/Validation/Validation';
+import AbstractPreUserRepository from 'src/Client/application/repositories/preUser/preUserRepository';
+import { Injectable } from '@nestjs/common';
+import ErrorUserAlreadyCreated from '../../../errors/userAlreadyCreated';
+import AbstractUserRepository from '../../../repositories/user/userRepository';
+import {
+  AbstractCreateValidationCodeUseCase,
+  CreateValidationCodeUseCase,
+} from '../../code-validation/create/create-validation-code';
+import PreUser from 'src/Client/application/entities/User/preUser';
+import { Password } from 'src/Client/application/entities/User/Password';
 
 interface IDataProps {
-    name: string;
-    email: string;
-    password: string;
+  name: string;
+  email: string;
+  password: string;
 }
 
 @Injectable()
 export class StartAccountUseCase {
-    constructor (
-        private readonly userRepository: AbstractUserRepository,
-        private readonly preUserRepository: AbstractPreUserRepository,
-        private readonly createValidationCodeUseCase: AbstractCreateValidationCodeUseCase
-    ) {}
+  constructor(
+    private readonly userRepository: AbstractUserRepository,
+    private readonly preUserRepository: AbstractPreUserRepository,
+    private readonly createValidationCodeUseCase: AbstractCreateValidationCodeUseCase,
+  ) {}
 
-    async execute(data: IDataProps): Promise<{ preUserId: string }> {
-        if (await this.userRepository.existsByEmail(data.email)) throw new ErrorUserAlreadyCreated("email");
-        if (await this.userRepository.existsByName(data.name)) throw new ErrorUserAlreadyCreated("name");
-        if (await this.preUserRepository.existsByName(data.name)) throw new ErrorUserAlreadyCreated("name");
+  async execute(data: IDataProps): Promise<{ preUserId: string }> {
+    if (await this.userRepository.existsByEmail(data.email))
+      throw new ErrorUserAlreadyCreated('email');
+    if (await this.userRepository.existsByName(data.name))
+      throw new ErrorUserAlreadyCreated('name');
+    if (await this.preUserRepository.existsByName(data.name))
+      throw new ErrorUserAlreadyCreated('name');
 
-        if (await this.preUserRepository.existsByEmail(data.email)) {
-            await this.preUserRepository.deleteByEmail(data.email);
-        }
-
-        const preUser = new PreUser({ 
-            name: data.name, 
-            email: data.email,
-            password_hash: new Password(data.password)
-        });
-        await this.preUserRepository.save(preUser);
-
-        await this.createValidationCodeUseCase.execute({ 
-            email: data.email, 
-            operationToValidateType: OperationToValidateTypeEnum.EMAIL_CONFIRMATION 
-        })
-
-        return { preUserId: preUser.id };
+    if (await this.preUserRepository.existsByEmail(data.email)) {
+      await this.preUserRepository.deleteByEmail(data.email);
     }
+
+    const preUser = new PreUser({
+      name: data.name,
+      email: data.email,
+      password_hash: new Password(data.password),
+    });
+    await this.preUserRepository.save(preUser);
+
+    await this.createValidationCodeUseCase.execute({
+      email: data.email,
+      operationToValidateType: OperationToValidateTypeEnum.EMAIL_CONFIRMATION,
+    });
+
+    return { preUserId: preUser.id };
+  }
 }
