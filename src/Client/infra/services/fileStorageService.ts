fd05a7664b36e15611b89048ee11b149a3aee888
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-08-16 16:36:25 -0300

    Implementing fileStorage

--- src/Client/infra/services/fileStorageService.ts
+++ src/Client/infra/services/fileStorageService.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import AbstractFileStorageService from 'src/Client/application/services/FileStorageService';
+import { AbstractGenericStorageProvider } from './s3Provider';
+import { MediaTypeEnum } from 'src/Client/application/use-cases/upload-media/upload-media-use-case';
+
+export interface IFileStorageUploadParams {
+  mediaType: MediaTypeEnum;
+  file: Buffer;
+  mimeType: string;
+}
 
 @Injectable()
 export default class FileStorageService implements AbstractFileStorageService {
-  upload(data: { path: string; file: Buffer }): Promise<{ url: string }> {
-    throw new Error('Method not implemented.');
+  constructor(private storageProvider: AbstractGenericStorageProvider) {}
+
+  async upload({
+    file,
+    mimeType,
+    mediaType,
+  }: IFileStorageUploadParams): Promise<{ url: string }> {
+    const path = `${mediaType}/image_${Date.now()}.${mimeType.split('/')[1]}`;
+
+    return this.storageProvider.upload({
+      file,
+      mimeType,
+      key: path,
+    });
   }
 }
