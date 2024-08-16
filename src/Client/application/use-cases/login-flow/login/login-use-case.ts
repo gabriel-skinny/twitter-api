1f02b598d2869b6c20d45b3b21fa143587b407d9
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-08-16 09:32:54 -0300

    Adding new functionality to loginUseCase

--- src/Client/application/use-cases/login-flow/login/login-use-case.ts
+++ src/Client/application/use-cases/login-flow/login/login-use-case.ts
@@ -1,43 +1,59 @@
-import UserSession, { SessionDeviceTypesEnum } from "src/Client/application/entities/User/Session";
-import ErrorUserNotFound from "src/Client/application/errors/userNotFound";
+import UserSession, {
+  SessionDeviceTypesEnum,
+} from 'src/Client/application/entities/User/Session';
+import ErrorUserNotFound from 'src/Client/application/errors/userNotFound';
 
-import AbstractSessionRepository from "src/Client/application/repositories/session/sessionRepository";
-import AbstractUserRepository from "src/Client/application/repositories/user/userRepository";
-import { AbstractAuthService } from "src/Client/application/services/AuthService";
-import { Injectable } from "@nestjs/common";
+import AbstractSessionRepository from 'src/Client/application/repositories/session/sessionRepository';
+import AbstractUserRepository from 'src/Client/application/repositories/user/userRepository';
+import { AbstractAuthService } from 'src/Client/application/services/AuthService';
+import { Injectable } from '@nestjs/common';
 
 interface ILoginUseCaseParams {
-    email: string; 
-    password: string; 
-    ip: string;
-    deviceType: SessionDeviceTypesEnum
+  email: string;
+  password: string;
+  ip: string;
+  deviceType: SessionDeviceTypesEnum;
 }
 
 @Injectable()
 export default class LoginUseCase {
-    constructor(
-        private readonly userRepository: AbstractUserRepository,
-        private readonly authService: AbstractAuthService,
-        private readonly userSessionRepository: AbstractSessionRepository
-    ) {}
+  constructor(
+    private readonly userRepository: AbstractUserRepository,
+    private readonly authService: AbstractAuthService,
+    private readonly userSessionRepository: AbstractSessionRepository,
+  ) {}
 
-    async execute({ email, password, ip, deviceType }: ILoginUseCaseParams): Promise<{ loginToken: string }> {
-        const user = await this.userRepository.findByEmail(email);
+  async execute({
+    email,
+    password,
+    ip,
+    deviceType,
+  }: ILoginUseCaseParams): Promise<{ loginToken: string }> {
+    const user = await this.userRepository.findByEmail(email);
 
-        if (!user) throw new ErrorUserNotFound();
+    if (!user) throw new ErrorUserNotFound();
 
-        if (!user.password_hash.isTheSameValue(password)) throw new Error("Wrong password");
+    if (!user.password_hash.isTheSameValue(password))
+      throw new Error('Wrong password');
 
-        const userSession = new UserSession({ 
-            userId: user.id,
-            ip,
-            deviceType
-        });
-        
-        await this.userSessionRepository.save(userSession);
+    if (
+      await this.userSessionRepository.findByUserIdAndIp({
+        userId: user.id,
+        ip,
+      })
+    )
+      throw new Error('User session already created');
 
-        const loginToken = await this.authService.makeLoginTokenToUser(user);
+    const userSession = new UserSession({
+      userId: user.id,
+      ip,
+      deviceType,
+    });
 
-        return { loginToken }
-    }
+    await this.userSessionRepository.save(userSession);
+
+    const loginToken = await this.authService.makeLoginTokenToUser(user);
+
+    return { loginToken };
+  }
 }
