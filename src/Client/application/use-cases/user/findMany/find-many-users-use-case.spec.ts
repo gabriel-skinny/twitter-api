e9b39366e5f4a1cef2049007cc758e7d932b962d
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-08-20 06:54:28 -0300

    Finishing find many users use-case

--- src/Client/application/use-cases/user/findMany/find-many-users-use-case.spec.ts
+++ src/Client/application/use-cases/user/findMany/find-many-users-use-case.spec.ts
@@ -20,13 +20,28 @@ describe('Find many users use case', () => {
     const user2 = makeUser();
     await userRepository.save(user2);
 
-    const response = await findManyUsersUseCase.execute({
-      page: 1,
-      perPage: 20,
-    });
+    const response = await findManyUsersUseCase.execute();
 
     expect(response.totalCount).toBe(2);
     expect(response.users).toHaveLength(2);
+  });
+
+  it('Should return only selected users', async () => {
+    const { userRepository, findManyUsersUseCase } = makeUseCaseSut();
+
+    const user = makeUser();
+    await userRepository.save(user);
+
+    const user2 = makeUser();
+    await userRepository.save(user2);
+
+    const response = await findManyUsersUseCase.execute({
+      page: 1,
+      perPage: 1,
+    });
+
+    expect(response.totalCount).toBe(2);
+    expect(response.users).toHaveLength(1);
   });
 
   it('Should throw a WrongValue error with the per Page and page values are higher than the total database', async () => {
--- src/Client/application/use-cases/user/findMany/find-many-users-use-case.ts
+++ src/Client/application/use-cases/user/findMany/find-many-users-use-case.ts
@@ -4,8 +4,8 @@ import WrongValueError from 'src/Client/application/errors/wrongValue';
 import AbstractUserRepository from 'src/Client/application/repositories/user/userRepository';
 
 interface IFindManyUserUseCaseParams {
-  page: number;
-  perPage: number;
+  page?: number;
+  perPage?: number;
   filter?: {
     name?: string | RegExp;
   };
@@ -21,13 +21,17 @@ interface IFindManyUsersUseCaseReturn {
 export class FindManyUsersUseCase {
   constructor(private userRepository: AbstractUserRepository) {}
 
-  async execute({
-    page = 1,
-    perPage = 20,
-    filter,
-    select,
-  }: IFindManyUserUseCaseParams): Promise<IFindManyUsersUseCaseReturn> {
-    const total = await this.userRepository.count(filter);
+  async execute(
+    data?: IFindManyUserUseCaseParams,
+  ): Promise<IFindManyUsersUseCaseReturn> {
+    const page = data?.page || 1;
+    const perPage = data?.perPage || 20;
+
+    Object.keys(data.filter).map(
+      (key) => data.filter[key] == undefined && delete data.filter[key],
+    );
+
+    const total = await this.userRepository.count(data?.filter);
     const skip = page * perPage - perPage;
 
     if (skip >= total) throw new WrongValueError('perPage');
@@ -35,8 +39,8 @@ export class FindManyUsersUseCase {
     const users = await this.userRepository.findMany({
       skip,
       limit: perPage,
-      filter,
-      select,
+      filter: data?.filter,
+      select: data?.select,
     });
 
     return {
