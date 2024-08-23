6d9570964fedad00c11897987e04952ee7d755a1
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-08-23 17:09:09 -0300

    Adding new method to base repository

--- src/Tweet/application/repositories/base.ts
+++ src/Tweet/application/repositories/base.ts
@@ -1,5 +1,12 @@
 import { BaseTweet, TweetTypesEnum } from '../entities/baseTweet';
 
+interface IFindManyPagination {
+  skip?: number;
+  limit?: number;
+  order?: string;
+  orderBy?: { id?: boolean; createdAt?: boolean };
+}
+
 export default abstract class AbstractBaseTweetRepository<T = BaseTweet> {
   abstract save(data: T): Promise<void>;
   abstract existsById(id: string): Promise<boolean>;
@@ -7,13 +14,14 @@ export default abstract class AbstractBaseTweetRepository<T = BaseTweet> {
   abstract deleteById(id: string): Promise<void>;
   abstract updateById(data: { id: string; data: Partial<T> }): Promise<void>;
   abstract findByUserId(userId: string): Promise<T | null>;
-  abstract findManyByUserId(data: {
-    userId: string;
-    skip?: number;
-    limit?: number;
-    order?: string;
-    orderBy?: { id?: boolean; createdAt?: boolean };
-  }): Promise<T[] | null>;
+  abstract findManyByParentId(
+    data: { parentId: string } & IFindManyPagination,
+  ): Promise<T[]>;
+  abstract findManyByUserId(
+    data: {
+      userId: string;
+    } & IFindManyPagination,
+  ): Promise<T[] | null>;
   abstract countByParentIdAndType(data: {
     parentId: string;
     tweetType: TweetTypesEnum;
