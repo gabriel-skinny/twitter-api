01197c3fccc93fcd318d34a98dcfeed719411721
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-08-26 08:25:02 -0300

    Adding new methods to cacheService

--- src/Tweet/application/services/cacheService.ts
+++ src/Tweet/application/services/cacheService.ts
@@ -1,4 +1,13 @@
 export abstract class AbstractCacheService {
   abstract set(data: { key: string; value: Object | string }): Promise<void>;
   abstract get(key: string): Promise<any>;
+  abstract setUseCaseResult<K extends string, T extends Object>(
+    data: {
+      [P in K]: string;
+    } & { useCaseResult: T },
+  ): Promise<void>;
+
+  abstract getUseCaseResult<K extends string, T extends Object>(data: {
+    [P in K]: string;
+  }): Promise<T>;
 }
