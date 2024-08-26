30fa546f6493677fde87942bef64d9cb34cba1f4
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-08-26 08:25:31 -0300

    Adding new methods on cacheService interface

--- src/Tweet/application/test/services/cacheService.ts
+++ src/Tweet/application/test/services/cacheService.ts
@@ -3,4 +3,13 @@ import { AbstractCacheService } from '../../services/cacheService';
 export class CacheServiceMock implements AbstractCacheService {
   async set(data: { key: string; value: Object | string }): Promise<void> {}
   async get(key: string): Promise<any> {}
+  async setUseCaseResult<K extends string, T extends Object>(
+    data: { [P in K]: string } & { useCaseResult: T },
+  ): Promise<void> {}
+
+  async getUseCaseResult<K extends string, T extends Object>(data: {
+    [P in K]: string;
+  }): Promise<T> {
+    return false as any;
+  }
 }
