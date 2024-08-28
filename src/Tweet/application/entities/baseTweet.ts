a7e3c6aaf0ce92b5a25e9e6998280d82c1105294
gabriel-skinny <gabrielcatoni12@gmail.com>
2024-08-28 09:34:07 -0300

    Updating baseTweet entity

--- src/Tweet/application/entities/baseTweet.ts
+++ src/Tweet/application/entities/baseTweet.ts
@@ -28,87 +28,37 @@ export interface IPropsBaseTweet {
   createdAt?: Date;
 }
 
-type IRawValues = Omit<
-  IPropsBaseTweet,
-  'id' | 'createdAt' | 'active' | 'likes'
->;
-
 export class BaseTweet {
-  private _id: string;
-  private _createdAt: Date;
-  private _active: boolean;
-  private _likes: LikeType;
-  private _rawValues: IRawValues;
+  id: string;
+  userId: string;
+  content: string;
+  type: TweetTypesEnum;
+  active: boolean;
+  mediaUrl?: string;
+  share?: Share[];
+  likes?: LikeType;
+  commentary?: Comment[];
+  creatorReferenceTweetId?: string;
+  parentId?: string;
+  parentType?: TweetTypesEnum;
+  createdAt: Date;
 
   constructor(props: IPropsBaseTweet) {
-    this._id = props.id || randomUUID();
-    this._createdAt = props.createdAt || new Date();
-    this._active = props.active || true;
-    this._likes = props.likes || {};
-
-    this._rawValues = { ...props };
-  }
-
-  public get id() {
-    return this._id;
-  }
-  public get userId() {
-    return this._rawValues.userId;
-  }
-
-  public get content() {
-    return this._rawValues.content;
-  }
-
-  public get type() {
-    return this._rawValues.type;
-  }
-
-  public get mediaUrl() {
-    return this._rawValues.mediaUrl;
-  }
-
-  public get share() {
-    return this._rawValues.share;
-  }
-
-  public get likes() {
-    return this._likes;
+    this.id = props.id || randomUUID();
+    this.createdAt = props.createdAt || new Date();
+    this.active = props.active || true;
+    this.likes = props.likes || {};
   }
 
   public like(requesterUserId: string) {
-    if (this._likes[requesterUserId]) throw new Error('Already liked');
-    else this._likes[requesterUserId] = true;
+    if (this.likes[requesterUserId]) throw new Error('Already liked');
+    else this.likes[requesterUserId] = true;
   }
 
   public deslike(requesterUserId: string) {
-    if (!this._likes[requesterUserId])
+    if (!this.likes[requesterUserId])
       throw new NotFoundCustomError('Like not found');
 
-    this._likes[requesterUserId] = false;
-  }
-
-  public get commentary() {
-    return this._rawValues.commentary;
-  }
-
-  public get active() {
-    return this._active;
-  }
-
-  public get creatorReferenceTweetId() {
-    return this._rawValues.creatorReferenceTweetId;
-  }
-
-  public get parentId() {
-    return this._rawValues.parentId;
-  }
-
-  public get parentType() {
-    return this._rawValues.parentType;
-  }
-
-  public get createdAt() {
-    return this._createdAt;
+    this.likes[requesterUserId] = false;
   }
 }
